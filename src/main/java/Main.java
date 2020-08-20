f0238155027e31d5d69a119c4b2c13594ec0b6b2
Alejandro Perez Gancedo <jandrito97@gmail.com>
2020-08-21 01:21:46 +0200

    Finished main code, missing tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,11 +17,11 @@ abstract class VersionString<T>{
 
 }
 class ServerString extends VersionString<ServerString>{
-    //String address;
+
 
     public ServerString(String version) {
         super(version);
-        System.out.println("Server String constructor");
+        //System.out.println("Server String constructor");
     }
 
     @Override
@@ -34,97 +34,70 @@ class ServerString extends VersionString<ServerString>{
     //Call getVersion to get the string and check if its valid or not for a comparaison
     @Override
     public boolean valid2(String version) {
-        System.out.println("I'm printing version "+version);
+        //System.out.println("I'm printing version "+version);
         Pattern pattern = Pattern.compile("((?:[0-9]{1,3}\\.?){0,2}[0-9]{1,3})(?:-\\w*)?", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(version);
         if (matcher.matches()){
-            System.out.println("True");
+            //System.out.println("True");
             return true;
         }else {
-            System.out.println("False");
+            //System.out.println("False");
             return false;
         }
 
     }
 
 
-
-
-
-    /*
-    @Override
-    public String toString() {
-        return address;
-    }
-
-     */
-
-    /*@Override
-    public boolean valid(VersionString str) {
-        System.out.println("------>"+str.getVersion());
-        return false;
-    }*/
 }
 // https://regex101.com/r/vR7yO4/15 capture the whole string to get if valid
 //capture groups for comparing
 public class Main {
     public static void main(String[] args) {
 
-        ServerString a = new ServerString("abc");
-        ServerString b = new ServerString("1.9.2");
+        ServerString a = new ServerString("1.8.8-alpha");
+        ServerString b = new ServerString("2.0.o");
         //a.valid(a);
         //b.valid(b);
-        b.valid2(b.getVersion());
-        System.out.println(formatForCompare(a.getVersion(), b.getVersion()));
-        /*System.out.println(a.getVersion());
+        if (b.valid2(b.getVersion()) && a.valid2(a.getVersion())){
+            compare(format(a.getVersion()), format(b.getVersion()), a, b);
+        }else if (!a.valid2(a.getVersion())){
+            System.out.println(a.getVersion()+" is invalid");
+        }else if(!b.valid2(b.getVersion())){
+            System.out.println(b.getVersion()+" is invalid");
+        }
+
+        //compare(format(a.getVersion()), format(b.getVersion()), a, b);
+        //System.out.println(formatForCompare(a.getVersion(), b.getVersion()));
+       ;
 
        // https://regex101.com/r/YLFEdl/1
-        Pattern pattern = Pattern.compile("((?:[0-9]{1,3}\\.?){0,2}[0-9]{1,3})(?:-\\w*)?", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher("1.9.8-alpha");
-        boolean matchFound = matcher.matches();
-        int nmatchs = matcher.groupCount();
-        if(matchFound) {
-            System.out.println("Match found");
-        } else {
-            System.out.println("Match not found");
-        }
-
-        if(nmatchs==1) {
-            System.out.println(matcher.group(nmatchs));
-        } else {
-            for (int i = 0; i < nmatchs; i++) {
-                System.out.println(matcher.group(i));
-
-            }
-        }
 
         //https://regex101.com/r/vR7yO4/15
-        Pattern patterngroups = Pattern.compile("(([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})|([0-9]{1,3})\\.([0-9]{1,3})|([0-9]{1,3}))(?:-\\w*)?", Pattern.CASE_INSENSITIVE);
-        Matcher matchergroups = patterngroups.matcher("1.9.8");
-        boolean matchFoundgroups = matchergroups.matches();
-        int nmatchsgroups = matchergroups.groupCount();
-        if(matchFoundgroups) {
-            System.out.println("Match found groups");
-        } else {
-            System.out.println("Match not found");
-        }
 
-        if(nmatchsgroups==1) {
-            System.out.println(matchergroups.group(nmatchsgroups));
-        } else {
-            for (int i = 0; i < nmatchsgroups; i++) {
-                System.out.println(matchergroups.group(i));
-
-            }
-        }
-*/
 
     }
     public static ArrayList<String> format (String version){
         ArrayList<String> ver = new ArrayList<>();
+        Pattern patterngroups = Pattern.compile("([0-9]{1,3})", Pattern.CASE_INSENSITIVE);
+        Matcher matcher1 = patterngroups.matcher(version);
+        while (matcher1.find()){
+            //starting on 1 because 0 is the whole string
+            for (int i = 1; i <= matcher1.groupCount(); i++) {
+                ver.add(matcher1.group(i));
+                //System.out.println(matcher1.group(i));
+            }
+        }
+        System.out.println(ver);
+        if(ver.size()<3){
+            while(ver.size()<3){
+                ver.add("0");
+            }
+        }
+        //System.out.println(ver);
         return ver;
     }
 
+    /*
     public static String formatForCompare(String version1, String version2){
         ArrayList<String> ver1= new ArrayList<String>();
         ArrayList<String> ver2= new ArrayList<String>();
@@ -160,22 +133,29 @@ public class Main {
             }
         }
         System.out.println(ver2);
-        compare(ver1, ver2);
+
         return String.valueOf(nmatchsgroups);
     }
 
-    public static String compare(ArrayList<String> compare1, ArrayList<String> compare2){
+     */
+
+    public static void  compare(ArrayList<String> compare1, ArrayList<String> compare2, ServerString a, ServerString b){
         int i= 0;
         while (i<compare1.size()){
             if (Integer.parseInt(compare1.get(i))>Integer.parseInt(compare2.get(i))){
+                System.out.println(a.getVersion()+" greater than "+b.getVersion());
+                break;
 
-                return "primer";
             }else if (Integer.parseInt(compare1.get(i))<Integer.parseInt(compare2.get(i))){
-                return "segundo";
+                System.out.println(b.getVersion()+" greater than "+a.getVersion());
+                break;
             }else{
                 i++;
             }
+            if (i==compare1.size()-1){
+                System.out.println(a.getVersion()+" is equal to "+b.getVersion());
+            }
         }
-        return "try";
+
     }
 }
