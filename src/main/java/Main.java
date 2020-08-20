6f5e2d5d9195a72dde6c73317a6ac38d7c6cabf0
Alejandro Perez Gancedo <jandrito97@gmail.com>
2020-08-20 02:21:59 +0200

    Added full regex, will need to deal with null from find method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ abstract class VersionString<T>{
 
     public abstract boolean valid(T str);
     public abstract boolean valid2(String version);
+    
 }
 class ServerString extends VersionString<ServerString>{
     //String address;
@@ -28,10 +29,42 @@ class ServerString extends VersionString<ServerString>{
         return false;
     }
 
+
+    //Call getVersion to get the string and check if its valid or not for a comparaison
     @Override
     public boolean valid2(String version) {
         System.out.println("I'm printing version "+version);
-        return false;
+        Pattern pattern = Pattern.compile("((?:[0-9]{1,3}\\.?){0,2}[0-9]{1,3})(?:-\\w*)?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(version);
+        if (matcher.matches()){
+            System.out.println("True");
+            return true;
+        }else {
+            System.out.println("False");
+            return false;
+        }
+
+    }
+    
+    public String compare(String version1, String version2){
+        String result = null;
+        Pattern patterngroups = Pattern.compile("([0-9]{1,3})|([0-9]{1,3})\\.([0-9]{1,3})|([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})", Pattern.CASE_INSENSITIVE);
+        Matcher matcher1 = patterngroups.matcher("1.8.8");
+        Matcher matcher2 = patterngroups.matcher("1.9.8");
+        int nmatchsgroups = matcher1.groupCount();
+        if(matcher1.matches()){
+            //reset to rematch all the string
+            matcher1.reset();
+            while (matcher1.find()){
+                //starting on 1 because 0 is the whole string
+                for (int i = 1; i <= matcher1.groupCount(); i++) {
+
+                    System.out.println(matcher1.group(i));
+                }
+            }
+        }
+
+        return String.valueOf(nmatchsgroups);
     }
 
     /*
@@ -54,11 +87,12 @@ public class Main {
     public static void main(String[] args) {
 
         ServerString a = new ServerString("abc");
-        ServerString b = new ServerString("cdb");
-        a.valid(a);
-        b.valid(b);
+        ServerString b = new ServerString("1.9.2");
+        //a.valid(a);
+        //b.valid(b);
         b.valid2(b.getVersion());
-        System.out.println(a.getVersion());
+        System.out.println(a.compare(a.getVersion(), b.getVersion()));
+        /*System.out.println(a.getVersion());
 
        // https://regex101.com/r/YLFEdl/1
         Pattern pattern = Pattern.compile("((?:[0-9]{1,3}\\.?){0,2}[0-9]{1,3})(?:-\\w*)?", Pattern.CASE_INSENSITIVE);
@@ -99,7 +133,7 @@ public class Main {
 
             }
         }
-
+*/
 
     }
 }
